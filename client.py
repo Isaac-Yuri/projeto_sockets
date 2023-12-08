c8aca7ec2e56a5e8f26e5e23b57bcd7543f7c477
Isaac Yuri <isaacyuri22@gmail.com>
2023-12-08 15:46:03 -0300

    arquivo da parte dos clientes criado e configurado inicialmente

---
+++ client.py
@@ -0,0 +1,18 @@
+import socket
+
+HOST = "localhost"
+PORT = 8080
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+cliente.connect((HOST, PORT))
+
+msg = input("Digite uma mensagem a ser enviada para o servidor: ")
+
+cliente.sendall(msg.encode())
+
+resposta = cliente.recv(1024)
+
+print(f"Resposta do servidor: {resposta.decode()}")
+
+cliente.close()
