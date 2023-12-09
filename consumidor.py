20120ccda324af99daaa7c3927dd7d8b6545537e
Isaac Yuri <isaacyuri22@gmail.com>
2023-12-09 20:04:43 -0300

    agora o consumidor pode finalizar ou cancelar a compra

--- consumidor.py
+++ consumidor.py
@@ -6,6 +6,11 @@ PORT = 8080
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cliente.connect((HOST, PORT))
 
+
+def linha(tamanho=40):
+    print("-"*tamanho)
+
+
 msg = input("Digite seu nome completo: ")
 cliente.sendall(msg.encode())
 
@@ -26,18 +31,25 @@ resposta = cliente.recv(1024).decode()
 
 produtos_comprados = []
 while True:
-    print("-"*40)
+    linha()
     print(resposta, end="")
 
     # Pega qual produto o cliente deseja comprar
-    id_produto = input("Digite qual produto deseja comprar pelo seu ID ou digite 0 para sair: ")
+    id_produto = input(
+        "Digite qual produto deseja comprar pelo seu ID ou digite 0 para finalizar: ")
     if id_produto == "0":
         break
     quantidade = int(input("Quantas unidades deseja comprar? "))
     produto = {"id": id_produto, "quantidade": quantidade}
+    confirmar = input(f"""Então você quer {produto['quantidade']} unidades do produto com id {produto['id']}?
+Digite 0 para confirmar ou 1 para cancelar a compra: """)
 
-    # Adiciona o produto na lista de compras
-    produtos_comprados.append(produto)
+    if confirmar == "0":
+        # Adiciona o produto na lista de compras
+        produtos_comprados.append(produto)
+        print("Produto adicionado com sucesso a lista de compras!")
+    else:
+        print("Compra cancelada!")
 
 # Transforma a lista de produtos comprados em string e envia para o servidor
 produtos_comprados = json.dumps(produtos_comprados)
